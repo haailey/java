5604ab4be55452bbe5744434822a532fe65b9473
Haailey <892051496@qq.com>
2015-06-05 21:50:36 -0400

    add hw3

---
+++ P1_16.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class P1_16
+{
+	public static void main(String[] agrs)
+	{
+			String name = JOptionPane.showInputDialog("My name is Hal! What would you like me to do?");
+			System.out.println("I'm sorry, Caro. I'm afraid I can't do that.");
+			System.exit(0);
+	}
+}
