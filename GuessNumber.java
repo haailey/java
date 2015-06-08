a0fc6b80f830468ae5b19b44397ad1cb422b4478
Haailey <892051496@qq.com>
2015-06-08 00:07:57 -0400

    unfinished hw

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,16 +1,58 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class GuessNumber
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Guess Number Game:");
-		System.out.println("Press 1Begin 2Exit");
-		Scanner s = new Scanner(System.in);
-		int ch;
-        int win = 0;
-        int lose = 0;
-        int dh = 0;
-        int fg;
-        ch = s.nextInt();
+		while(true)
+		{
+			System.out.println("Welcome to Guess Number Game:");
+			System.out.println("Press 1 Begin 2 Exit");
+			Scanner s = new Scanner(System.in);
+			int ch = 0;
+        	ch = s.nextInt();
+        	if(ch==1)
+        	{
+        		System.out.println("--------------");
+        		Scanner ca = new Scanner(System.in);
+        		int max = 0;
+        		int input = 0;
+        		int i = 0;
+        		max = (int)(Math.random()*100);
+        		do
+        		{
+        			System.out.println("Please enter a number:");
+        			input = ca.nextInt();
+        			if(input < max)
+        			{
+        				System.out.println("It's too low!");
+        				if(input > max)
+        				{
+        					System.out.println("It's too high!");
+        					{
+        						if(input==max)
+        						{
+        							break;
+        							{
+        								i++;
+        								while(i > 10);
+        								switch(i)
+        								{
+        									default: System.out.println("Sorry,you've used all your chances");
+											{
+												if(ch==2); {break;}
+												{
+													System.out.println("Thank you!");		
+												}
+											}
+        								}
+        							}
+        						}
+        					}
+						}
+        			}
+        		}
+        	}
+		}	
 	}
 }
