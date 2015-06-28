4faefbcd708f4f42665015977e7103c64dd7e2ea
Haailey <892051496@qq.com>
2015-06-28 04:35:12 -0400

    Add unfinished hw

--- testmedian.java
+++ testmedian.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class testmedian
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		double[] data = new double[10];
-		double[] moreData = { 32, 53, 64, 54, 64};
-		moreData[0] = 5;
+		Scanner sc = new Scanner(System.in);
+		int array[] = new int[10]; 
+		int c = 0;
+
+		for (int input = 0; input < array.length; input++)
 		{
-			System.out.println(moreData[0]);
+			System.out.print("Please input number" + (input + 1) + "grade");
+			array[input] = sc.nextInt();
+			int length = array.length;
+			int[] sort = new int[length];	
+			System.arraycopy(array, 0, sort, 0, sort.length);
+
+			        //Arrays.sort(sort);
+
+			if (length % 2 == 0)
+			{
+				c = (sort[(sort.length/2) - 1] + sort[sort.length / 2]) / 2;
+				{
+					System.out.println(c);
+				}
+			}
 		}
 	}
+    
 }
---
+++ week3.md
@@ -0,0 +1,4 @@
+# Homework
+- watch youtube vedios [4-12!!](https://www.youtube.com/watch?v=Hl-zzrqQoSE&list=PLFE2CE09D83EE3E28)
+	- Take notes
+- 中位数，并push到gitHub,在github上注释（在关键行）
